fa5d6ef49defc562f9115725dce7a1e723d20530
qff233 <347633553@qq.com>
2025-09-21 16:37:05 +0800

    调整项目架构

---
+++ server/src/main.rs
@@ -0,0 +1,60 @@
+use axum::{
+    Router,
+    http::{HeaderValue, Method},
+};
+use sqlx::postgres::PgPoolOptions;
+use tower_http::{cors::CorsLayer, trace::TraceLayer};
+use tracing::{info, level_filters::LevelFilter};
+use tracing_subscriber::{fmt, prelude::*};
+
+fn init_tracing() -> tracing_appender::non_blocking::WorkerGuard {
+    let file_appender = tracing_appender::rolling::daily("logs", "server");
+    let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
+
+    let file_layer = fmt::layer()
+        .with_ansi(false)
+        .with_writer(non_blocking)
+        .with_filter(LevelFilter::DEBUG);
+    let stdout_layer = fmt::layer()
+        .with_level(true)
+        .with_writer(std::io::stdout)
+        .with_filter(LevelFilter::INFO);
+
+    let collector = tracing_subscriber::registry()
+        .with(file_layer)
+        .with(stdout_layer);
+    tracing::subscriber::set_global_default(collector).expect("Tracing collect error");
+    guard
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let _guard = init_tracing();
+    info!("Starting server...");
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    info!("Connecting to database: {}", database_url);
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Failed to create pool");
+    info!("Database pool created successfully");
+
+    let cors_layer = CorsLayer::new()
+        .allow_origin("*".parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::GET, Method::POST])
+        .allow_headers([axum::http::header::CONTENT_TYPE]);
+    let trace_layer = TraceLayer::new_for_http();
+    let app = Router::new()
+        // .route("/sql", post(handle_sql_request))
+        .layer(trace_layer)
+        .layer(cors_layer)
+        .with_state(pool);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
+}
